f209ea24ea99ecd0292f7ae5e6b98d133e929076
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-05-14 11:00:32 -0400

    Built a method that finds the minimum key value of a hash without using keys,values,min,sort or min_by.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  prev = nil
+  if name_hash == {}
+    nil
+  else
+  name_hash.each do |key,value|
+    if prev == nil || prev >= value
+      prev = value
+    end
+  end
+    name_hash.each do |key, value|
+      if value == prev
+        return key
+      end
+    end
+  end
 
 end
